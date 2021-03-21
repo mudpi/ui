c14785792691e8d476320144418caf7d46547527
Eric Davisson <eric@mudpi.app>
2021-03-20 23:18:20 -0500

    Updates to display.js

--- app/js/displays.js
+++ app/js/displays.js
@@ -9,31 +9,31 @@ displays.forEach(display => {
 		formdata.append("message", display.querySelector('[name="message"]').value);
 		formdata.append("duration", display.querySelector('[name="duration"]').value);
 		formdata.append("topic", display.querySelector('[name="topic"]').value);
-		makeRequest('ajax/display_message.php', 'POST', formdata);
+		makeRequest('ajax/display_message.php', 'POST', formdata, display.querySelector('.send_message'));
 	});
 });
   
-function makeRequest(url, type = 'POST' , data = null, callback = null) {
+function makeRequest(url, type = 'POST' , data = null, callback = null, button_element = null) {
 	request = new XMLHttpRequest();
 
 	if (!request) {
 		//Problem making request
-	  	return false;
+		return false;
 	}
 	if (data === null) {
 		console.log("Defaulting form data.");
 		data = new FormData();
 	}
 
-	button.textContent = "Sending...";
-	button.disabled = true;
-	button.classList.add('is-grey');
-	button.classList.remove('is-primary');
-  	errors_list.classList.remove("bg-red-light");
-  	errors_list.classList.remove("p-2");
+	button_element.textContent = "Sending...";
+	button_element.disabled = true;
+	button_element.classList.add('is-grey');
+	button_element.classList.remove('is-primary');
+	errors_list.classList.remove("bg-red-light");
+	errors_list.classList.remove("p-2");
 
 	if (callback === null) {
-		request.onreadystatechange = handleResponse;
+		request.onreadystatechange = handle_response(button_element);
 	}
 	else {
 		request.onreadystatechange = callback;
@@ -44,40 +44,42 @@ function makeRequest(url, type = 'POST' , data = null, callback = null) {
 	request.send(data);
 }
 
-function handleResponse() {
-	if (request.readyState === XMLHttpRequest.DONE) {
-	  if (request.status === 200 || request.status === 201) {
-  		var response = JSON.parse(request.responseText).data;
-  		console.log(response);
-    	//Request successful
-		button.textContent = "Send Message";
-		button.disabled = false;
-		button.classList.remove('is-grey');
-		button.classList.add('is-primary');
+function handle_response(button_element = null) {
+	return function() {
+		if (request.readyState === XMLHttpRequest.DONE) {
+		  if (request.status === 200 || request.status === 201) {
+			var response = JSON.parse(request.responseText).data;
+			console.log(response);
+			//Request successful
+			button_element.textContent = "Send Message";
+			button_element.disabled = false;
+			button_element.classList.remove('is-grey');
+			button_element.classList.add('is-primary');
 
-	  } else {
-	  	var response = JSON.parse(request.responseText);
-	  	errors_list.classList.add("bg-red-light");
-	  	errors_list.classList.add("p-2");
-	  	let errors = `<p>${response.message}</p><ul>`;
-	  	for (var e in response.errors) {
-	  		errors += `<li class="text-xs">${e} - ${response.errors[e][0]}</li>`;
-	  	}
-	  	errors += `</ul>`;
-	  	errors_list.innerHTML = errors;
-		button.disabled = false;
-		button.textContent = "Create Account";
-		button.classList.remove('is-grey');
-		button.classList.add('is-white');
-    	//Problem with the request (500 error)
-	  }
-	}
+		  } else {
+			var response = JSON.parse(request.responseText);
+			errors_list.classList.add("bg-red-light");
+			errors_list.classList.add("p-2");
+			let errors = `<p>${response.message}</p><ul>`;
+			for (var e in response.errors) {
+				errors += `<li class="text-xs">${e} - ${response.errors[e][0]}</li>`;
+			}
+			errors += `</ul>`;
+			errors_list.innerHTML = errors;
+			button_element.disabled = false;
+			button_element.textContent = "Create Account";
+			button_element.classList.remove('is-grey');
+			button_element.classList.add('is-white');
+			//Problem with the request (500 error)
+		  }
+		}
+	};
 }
 
 function setCSRFHeader(xhr, type, d) {
-    var csrfToken = document.querySelector('meta[name=csrf_token]').getAttribute('content');
-    if (/^(POST|PATCH|PUT|DELETE)$/i.test(type)) {
-    	d.append("csrf_token", csrfToken);
-        xhr.setRequestHeader("X-CSRF-Token", csrfToken);
-    }
+	var csrfToken = document.querySelector('meta[name=csrf_token]').getAttribute('content');
+	if (/^(POST|PATCH|PUT|DELETE)$/i.test(type)) {
+		d.append("csrf_token", csrfToken);
+		xhr.setRequestHeader("X-CSRF-Token", csrfToken);
+	}
 }
